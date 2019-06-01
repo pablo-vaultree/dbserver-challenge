6a6c163a2070b6f5920a37016b1bc0cd0a39a127
Pablo Feijó <ruanmyv@gmail.com>
2019-06-01 02:03:17 -0300

    formating file

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-git pull origin master && docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build -d
+
+git pull origin master
+
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build -d
+
 chmod +x deploy.sh
 alias deploy.sh='sh deploy.sh'
+
 export PATH=$PATH:$PWD/deploy.sh
