ff1fdd86d6fb09f6b876f86aa7975571075fe8a4
Pablo Feijó <ruanmyv@gmail.com>
2019-06-01 02:02:10 -0300

    fixing path

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git pull origin master && docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build -d
-chmod +x deply.sh
-export PATH=$PWD/deply.sh
+chmod +x deploy.sh
+alias deploy.sh='sh deploy.sh'
+export PATH=$PATH:$PWD/deploy.sh
