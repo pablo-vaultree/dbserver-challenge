35cedd50a62692b4f59b007f015ea677c8524ce3
Pablo Feijó <ruanmyv@gmail.com>
2019-06-01 01:58:51 -0300

    fixing deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git pull origin master && docker-compose -f .\docker-compose.yml -f .\docker-compose.prod.yml up --build -d
+git pull origin master && docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build -d
 chmod +x deply.sh
 export PATH=$PWD/deply.sh
