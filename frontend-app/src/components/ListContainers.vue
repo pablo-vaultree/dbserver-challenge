9d29f7f101b9053b660c1d931eb3c665292f7abf
Pablo Feijó <ruanmyv@gmail.com>
2019-06-01 21:14:05 -0300

    updating style

--- frontend-app/src/components/ListContainers.vue
+++ frontend-app/src/components/ListContainers.vue
@@ -5,35 +5,40 @@
         <div class="row">
           <div class="col-md-12">
             <br>
+
             <h3>Containers List</h3>
             <br>
-            <div class="card-deck">
-            <template v-for="container in containers">
-              <ul :key="container.id" class="d-lg-none">
-                  <li>
-                    <p :class="container.temperatureWarning && 'text-danger'">
-                        {{ container.beerType }} beers | Current Temperature {{container.currentTemperature}}º
-                    </p>
-                  </li>
-              </ul>
 
-              <div :key="container.id" class="card mb-3 d-none d-lg-block" style="max-width: 18rem;">
-                <div class="card-header">
-                  {{ container.beerType }} beers
-                  <span v-if="container.temperatureWarning" class="badge badge-pill badge-danger float-right">Danger</span>
-                </div>
-                <div :class="container.temperatureWarning && 'text-danger'" class="card-body">
-                  <h5 class="card-title">Current Temperature {{container.currentTemperature}}º</h5>
-                  <p class="card-text">
-                      <ul>
-                        <li>deliver id {{container.deliverId}}</li>
-                        <li>beerCount {{container.beerCount}}</li>
-                      </ul>
+            <ul class="d-lg-none">
+              <template v-for="container in containers">
+                <li :key="container.id" >
+                  <p :class="container.temperatureWarning && 'text-danger'">
+                      {{ container.beerType }} beers | Temperature {{container.currentTemperature}}º
                   </p>
-                </div>
+                </li>
+              </template>
+            </ul>
+
+            <div class="d-none d-lg-block">
+              <div class="card-deck ">
+                <template v-for="container in containers">
+                  <div :key="container.id" class="card mb-3 text-center" style="min-width: 16rem;max-width: 16rem;">
+                    <div class="card-header">
+                      {{ container.beerType }} Container
+                      <span v-if="container.temperatureWarning" class="badge badge-pill badge-danger float-right">Danger</span>
+                    </div>
+
+                    <div :class="container.temperatureWarning && 'text-danger'" class="card-body">
+                      <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Temperature <br>{{container.currentTemperature}}º</li>
+                        <li class="list-group-item">BeerCount <br>{{container.beerCount}}</li>
+                      </ul>
+                    </div>
+                  </div>
+                </template>
               </div>
-            </template>
             </div>
+
           </div>
         </div>
       </div>
